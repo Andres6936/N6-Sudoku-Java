782bef41233ee96a5158fdeb6226bf120fc1b889
Joan Andrés <andres6936@live.com>
2017-11-09 09:53:52 -0500

    Update PanelImagen.java

--- src/edu/jabs/sudoku/interfaz/PanelImagen.java
+++ src/edu/jabs/sudoku/interfaz/PanelImagen.java
@@ -1,5 +1,38 @@
 package edu.jabs.sudoku.interfaz;
 
-public class PanelImagen
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+public class PanelImagen extends JPanel
 {
+
+    // -------------------------------------------
+    // Constructor
+    // -------------------------------------------
+
+    /**
+     * Serial por default.
+     */
+    private static final long serialVersionUID = 2L;
+
+    /**
+     * Constructor de la clase.
+     */
+    public PanelImagen( )
+    {
+        JLabel imagen = new JLabel( );
+        ImageIcon icono = new ImageIcon( "./data/imagenes/title.png" );
+        // El Label es añadido.
+        imagen = new JLabel( "" );
+        imagen.setIcon( icono );
+        add( imagen );
+
+        setBackground( Color.BLACK );
+        setBorder( new LineBorder( Color.BLACK ) );
+    }
+
 }
